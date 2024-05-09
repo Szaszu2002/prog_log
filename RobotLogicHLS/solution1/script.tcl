54164d58a177e983ccd488ec565be841d941023f
Szabó Alexandra Marianna <90461871+Szaszu2002@users.noreply.github.com>
2024-05-09 13:44:45 +0200

    hls

---
+++ RobotLogicHLS/solution1/script.tcl
@@ -0,0 +1,19 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+## Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+############################################################
+open_project RobotLogicHLS
+set_top robotControl
+add_files RobotLogicHLS/src/RobotLogic.c
+add_files RobotLogicHLS/src/RobotLogic.h
+open_solution "solution1" -flow_target vivado
+set_part {xc7a100tcsg324-1}
+create_clock -period 10 -name default
+config_export -output C:/Users/au087680/Desktop/-p/RobotLogicHLS/IP
+#source "./RobotLogicHLS/solution1/directives.tcl"
+#csim_design
+csynth_design
+#cosim_design
+export_design -rtl verilog -format ip_catalog -output C:/Users/au087680/Desktop/-p/RobotLogicHLS/IP
